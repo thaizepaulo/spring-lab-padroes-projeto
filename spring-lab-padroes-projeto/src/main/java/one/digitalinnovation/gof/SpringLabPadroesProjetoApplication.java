5156e769d53a30bb005c83f0233c4739af16be71
Thaize Paulo <thaizepaulo@gmail.com>
2023-09-11 10:20:25 -0300

    Criando o Projeto Spring Boot

---
+++ spring-lab-padroes-projeto/src/main/java/one/digitalinnovation/gof/SpringLabPadroesProjetoApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLabPadroesProjetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLabPadroesProjetoApplication.class, args);
+	}
+
+}
